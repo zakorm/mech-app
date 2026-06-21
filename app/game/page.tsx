b5974caad15c80dd5424ae3d3e424d3b533561de
Patanin N <patanin.paul.naenna@gmail.com>
2026-06-21 15:42:55 +0700

    debug4

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,15 +1,28 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import UnityGame from '../../components/UnityGame'
+
 export default function Home() {
-	return(
+  return (
 		<>
-
-			<div className="flex justify-center items-center h-screen">
-				<UnityGame />
+		
+			<div style={{
+				display: 'flex', 
+				justifyContent: 'center', 
+				alignItems: 'center', 
+				width: '100vw', 
+				height: '100vh', 
+				overflow: 'hidden',
+				position: 'fixed',
+				top: 0,
+				left: 0,
+			}}>
+				<iframe
+					src="/Unity/index.html"
+					style={{ width: '820px', height: '1160px', border: 'none' }}
+				/>
 			</div>
-
-					
+			<Link href="app">Back</Link>			
 		</>
-	)
+
+  )
 }
